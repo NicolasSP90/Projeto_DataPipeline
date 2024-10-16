b27b4276b0b448ffaedce8230c8b8ea4b80404b6
MarceloHenriques <marcelohenriques2003@gmail.com>
2024-10-16 16:24:22 -0300

    Crianção da funções para da pipeline

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -20,9 +20,6 @@ def get_date_shape(df):
     df = pd.read_parquet("../data/data_raw.parquet")
 
 
-
-
-
 def extract_font(df):
     def nome_fonte(fonte):
         return fonte.get('name')
@@ -30,3 +27,57 @@ def extract_font(df):
     df['source'] = df['source'].apply(nome_fonte)
 
     return df
+
+def verificar_e_excluir_duplicadas(df):
+    duplicados = df[df.duplicated(keep=False)]
+    
+    num_duplicados = df.duplicated().sum()
+    
+    if num_duplicados > 0:
+        print(f"Total de linhas duplicadas: {num_duplicados}")
+        print(duplicados)
+        
+        df = df.drop_duplicates()
+        print("Linhas duplicadas removidas com sucesso.")
+    else:
+        print("Não foram encontradas linhas duplicadas no DataFrame.")
+    
+    return df
+
+def valores_nulos(df):
+    nulos = df.isnull().sum()
+    print (nulos)
+
+
+def remover_coluna(df):
+    return df.drop(columns=['urlToImage'])
+
+def alterar_nome_colunas(df):
+    renomear_colunas = {
+        'author': 'Autor',
+        'title': 'Título',
+        'description': 'Descrição',
+        'url' : 'URL',
+        'publishedAt': 'Data Publicação',
+        'content': 'Conteúdo',
+        'source': 'Fonte'
+}
+    df = df.rename(columns=renomear_colunas)
+    return df
+    
+def organizar_colunas(df):
+    ordenamento_colunas = ['Data Publicação', 'Título', 'Autor', 'Descrição', 'URL', 'Fonte', 'Conteúdo']
+
+    df = df[ordenamento_colunas]
+    return df
+
+def data (df):
+    df['Data Publicação'] = pd.to_datetime(df['Data Publicação'], format='ISO8601')
+    df['Data Publicação'] = df['Data Publicação'].dt.strftime('%Y/%m/%d')
+    
+    return df
+
+def ordenar_data(df):
+    df = df.sort_values('Data Publicação').reset_index().drop(columns=['index'])
+    return df
+
